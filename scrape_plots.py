a5f1abb6a8bd0faa265629ed7ffbf3545f7d163e
Chelsea Ernhofer <cernhofer@cs.uchicago.edu>
2017-12-01 14:42:18 -0600

    updated plot scraper

--- scrape_plots.py
+++ scrape_plots.py
@@ -3,33 +3,39 @@ import requests
 import pdb
 import re
 import pandas as pd 
-
+from socket import error as SocketError 
+import time 
 
 white_space_pattern = re.compile(r'/n')
 movies = {'title': [], 'id': [], 'plot': []}
 
-for i in range(4080000, 6000000):
-	print('\n\n', i)
+for i in range(4435000, 6000000):
 	url = "http://www.imdb.com/title/tt{}/plotsummary?ref_=tt_stry".format(i)
-	r = requests.get(url)
-	bs = BeautifulSoup(r.text)
 	try: 
+		r = requests.get(url)
+		bs = BeautifulSoup(r.text)
 		main = bs.find('div', {'class': "subpage_title_block"})
 		title = main.findAll('a')[-1].text
 		#print(title)
 		#title = title[0].text
-		print(title)
 		plot = bs.findAll('ul', {'id': "plot-synopsis-content"})[0].text.strip()
 		if not plot.startswith("It looks like we don't have a Synopsis"):
+			print('\n\n', i)
+			print(title)
 			print(plot)
 			movies['title'].append(title)
 			movies['id'].append(i)
 			movies['plot'].append(plot)
+	except SocketError :
+		print("\n\n\n ERROR \n\n\n")
+		time.sleep(60)
+		pass
 	except AttributeError:
 		pass
 
+
 	if i % 5000 == 0 :
-		file_name = "movies4_{}.csv".format(i)
+		file_name = "movies23_{}.csv".format(i)
 		df = pd.DataFrame(movies)
 		df.to_csv(file_name)
 
